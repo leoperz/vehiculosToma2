28b5f21db760c6e935ed15f7f4b9e5bdd49db7f3
leoperz7 <jleoperz@gmail.com>
2018-05-19 00:01:31 -0300

    se agrego dos providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { CarritoProvider } from '../providers/carrito/carrito';
+import { UsuarioProvider } from '../providers/usuario/usuario';
 
 @NgModule({
   declarations: [
@@ -24,7 +26,9 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    CarritoProvider,
+    UsuarioProvider
   ]
 })
 export class AppModule {}
